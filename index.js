b1e0989aae3233a3953ae03c792be6da5fa98df5
Umair <umairsajid0017@yahoo.com>
2024-04-16 23:36:14 +0500

    First commit

--- index.js
+++ index.js
@@ -8,16 +8,30 @@ const server = http.createServer((req, res) => {
 
 const io = socketIo(server);
 
+const branchIds = new Map(); //convert it into map key,value pair
+
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    const branchId = socket.handshake.query.branchId;
+    if (!branchIds.has(branchId)) {
+        branchIds.set(branchId, new Set());
+    }
+    branchIds.set(branchId, socket.id);
+    console.log(`User ${socket.id} joined branch ${branchId}`);
+    console.log(branchIds);
     
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
     
-    socket.on('chat message', (msg) => {
-        console.log('Message:', msg);
-        io.emit('chat message', msg);
+    socket.on('notification', (msg) => {
+        const parsedMsg = JSON.parse(msg);
+        const branchId = parsedMsg.branchId;
+        console.log('Json:', parsedMsg, branchId);
+        const socketId = branchIds.get(branchId);
+        if (socketId) {
+            io.to(socketId).emit('orderReceived', msg);
+            console.log('notification sent to ' + socketId,msg);
+        }
     });
 });
 
